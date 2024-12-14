576763e7c9e1797210e090680df85f41a52fac4c
ViktorSouza <98527609+ViktorSouza@users.noreply.github.com>
2024-12-14 11:48:42 -0300

    docs: add Jogo class documentation

--- jogo.py
+++ jogo.py
@@ -1,3 +1,9 @@
+## @file jogo.py
+## Implementação da classe Jogo, que gerencia o funcionamento geral de um jogo estilo Tetris.
+#
+# Este arquivo define a classe Jogo, que oferece funcionalidades como iniciar novas partidas,
+# carregar partidas salvas, salvar pontuações e interagir com o usuário.
+
 from partida import Partida
 import json
 import pickle
@@ -8,10 +14,14 @@ from peca import Peca
 
 
 class Jogo:
+    ## Constrói um objeto Jogo.
+    # Inicializa o jogo tendo, como a partida atual, None.
     def __init__(self):
+        # Partida que será jogada pelo usuário
         self.current_game = None
-        pass
 
+    ## Imprime as opções disponíveis para o jogo, tais como iniciar uma partida ou sair do jogo.
+    #  @param self Ponteiro para o próprio objeto.
     def display_options(self):
         print("- <i> para iniciar uma nova partida")
         print("- <c> para carregar uma partida gravada e continuá-la")
@@ -28,9 +38,11 @@ class Jogo:
         elif tecla == "p":
             self.top_10_scores()
         elif tecla == "s":
-            print("Saindo do jogo...")
-            os.abort()
+            self.exit()
 
+    ## Inicia uma nova partida.
+    # Para iniciar uma nova partida, é necessário que o usuário digite, em linhas diferentes, o nome que será utilizado para salvar a partida e o número de linhas e colunas para o jogo.
+    #  @param self Ponteiro para o próprio objeto.
     def new_game(self):
         name = input("Digite o nome do jogador:")
         rows = int(input("Digite o número de linhas da tela do jogo:"))
@@ -47,11 +59,17 @@ class Jogo:
         self.play()
         self.display_options()
 
+    ## Salva a partida atual em um arquivo.
+    # A partida atual será salvada em um arquivo, possibilitando que o usuário continue a jogar a última partida mesmo que o terminal seja fechado.
+    #  @param self Ponteiro para o próprio objeto.
     def save_game(self):
         # Salvando o objeto em um arquivo
         with open("partida.pkl", "wb") as arquivo:
             pickle.dump(self.current_game, arquivo)
 
+    ## Imprime as dez melhores pontuações.
+    # Imprime, em ordem decrescente, as dez melhores pontuações dos usuários. Nota-se que um mesmo jogador pode aparecer no placar diversas vezes.
+    #  @param self Ponteiro para o próprio objeto.
     def top_10_scores(self):
         print("Lista das 10 melhores pontuações:")
         with open("scores.json", "r") as f:
@@ -64,6 +82,9 @@ class Jogo:
 
         self.display_options()
 
+    ## Salva o placar da partida atual.
+    # Após o jogador finalizar a partida, a sua pontuação é salva em um arquivo, no qual se encontra todas as pontuações de todos os jogadores. Este arquivo será utilizado para imprimir as melhores pontuações.
+    #  @param self Ponteiro para o próprio objeto.
     def save_score(self):
         if not os.path.exists("scores.json"):
             # Se o arquivo não existir, será criado com uma lista vazia
@@ -83,6 +104,9 @@ class Jogo:
             json.dump(res, f, indent=4)
             f.truncate()
 
+    ## Executa o loop principal de uma partida.
+    # Responsável por exibir o estado atual do jogo e capturar as ações do jogador. Além disso, a função é responsável pela detecção da finalização de uma partida.
+    #  @param self Ponteiro para o próprio objeto.
     def play(self):
         os.system("cls||clear")
         self.current_game.screen.draw_tetromino(self.current_game.current_tetromino)
@@ -137,5 +161,9 @@ class Jogo:
             print("<s> sai da partida, <g> grava e sai da partida")
         self.display_options()
 
+    ## Sai do jogo.
+    # Função responsável pelo tratamendo da saída do usuário, isto é, a finalização do jogo no terminal.
+    # @param self Ponteiro para o próprio objeto.
     def exit(self):
-        pass
+        print("Saindo do jogo...")
+        os.abort()
